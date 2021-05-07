cf8252101a0a15c3a5a3c9cea7dce30b90b4d769
azim2007 <azim040407@yandex.ru>
2021-05-07 13:12:27 +0400

    головоломка и взятие свечи

---
+++ Assets/scr/ToTakeObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToTakeObjects : MonoBehaviour {
+
+	public GameObject ItSelf;
+	public Image image;
+
+	void OnMouseUpAsButton(){
+		Debug.Log ("взятие свечи.");
+		IMAGEMASSIV.size++;
+		System.Array.Resize<Image> (ref IMAGEMASSIV.inventar, IMAGEMASSIV.size); //без увеличения размера массива оно не работало
+		IMAGEMASSIV.inventar[IMAGEMASSIV.inventar.Length - 1] = image;
+		Destroy (ItSelf);
+	}
+
+}
---
+++ Assets/scr/script1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour {
+	public static GameObject[] golov = new GameObject [10];
+	public static int CountOfSolveGames = 0;
+	public static bool IsGameSolved = false;
+
+	// Use this for initialization
+	void Start () {
+		golov[0] = GameObject.Find ("golovolomka");
+		golov[0].SetActive (false);
+	}
+	void Update(){
+		if(CountOfSolveGames == 1 && IsGameSolved){
+			golov [0].SetActive (false);
+			Debug.Log ("вывод второго диалога");
+			IsGameSolved = false;
+		}
+		//здесь остальные ифы для включения последующих диалогов
+		//CountOfGameSolved и IsGameSolved изменяются в скрипте GameController на 249 строчке
+	}
+}
